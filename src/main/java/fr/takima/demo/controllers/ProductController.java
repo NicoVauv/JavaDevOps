11a287808ff77faddb39d90c5d82e079954d973a
unknown <qbonnefond@gmail.com>
2019-09-17 11:52:06 +0200

    Front Q v1.6

--- src/main/java/fr/takima/demo/controllers/ProductController.java
+++ src/main/java/fr/takima/demo/controllers/ProductController.java
@@ -3,6 +3,7 @@ package fr.takima.demo.controllers;
 import fr.takima.demo.repositories.FridgeDAO;
 import fr.takima.demo.repositories.ListDAO;
 import fr.takima.demo.repositories.ProductDAO;
+import fr.takima.demo.repositories.ProductFridgeDAO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -18,10 +19,12 @@ public class ProductController {
   private final ListDAO ListDAO;
   private final FridgeDAO fridgeDAO;
 
+
   public ProductController(ProductDAO productDAO, ListDAO ListDAO, FridgeDAO fridgeDAO) {
     this.productDAO = productDAO;
     this.ListDAO = ListDAO;
     this.fridgeDAO = fridgeDAO;
+
   }
 
   // Get products from home page
@@ -32,6 +35,7 @@ public class ProductController {
     m.addAttribute("myfridge", fridgeDAO.findAll());
       return "index";
   }
+
 
   /*@PostMapping("/new")
   public RedirectView createNewUser(@ModelAttribute Product user, RedirectAttributes attrs) {
---
+++ src/main/java/fr/takima/demo/repositories/ProductFridgeDAO.java
@@ -0,0 +1,8 @@
+package fr.takima.demo.repositories;
+
+import fr.takima.demo.models.MyList;
+import fr.takima.demo.models.ProductFridge;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductFridgeDAO extends CrudRepository<ProductFridge, Long> {
+}
