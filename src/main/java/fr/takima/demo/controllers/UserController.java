0ed1f6cdc14f005bf2827f56b0e3cf30f61c5f89
Nicolas Vauvillier <nicolas.vauvillier@epfedu.fr>
2019-09-17 14:09:16 +0200

    Login + Registration

--- src/main/java/fr/takima/demo/controllers/UserController.java
+++ src/main/java/fr/takima/demo/controllers/UserController.java
@@ -1,14 +1,16 @@
 package fr.takima.demo.controllers;
 
+import fr.takima.demo.models.User;
 import fr.takima.demo.repositories.UserDAO;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 /**
  *
  */
-@RequestMapping("/login")
+@RequestMapping("/")
 @Controller
 
 public class UserController {
@@ -19,14 +21,42 @@ public class UserController {
         UserDAO = userDAO;
     }
 
-    @GetMapping
-    public String showAll(Model m) {
-        m.addAttribute("users", UserDAO.findAll());
-        return "login";
+    @PostMapping("/login")
+    public RedirectView getConnection(@ModelAttribute User user, RedirectAttributes attrs){
+        if(checkConnection(user)){
+            attrs.addFlashAttribute("message", "Connection Successed");
+            return new RedirectView("/home");
+        }
+        else {
+            attrs.addFlashAttribute("message", "Connection Failed");
+            return new RedirectView("/login");
+        }
     }
-    @GetMapping("/signin")
-    public String registration (Model m) {
-        m.addAttribute("users", UserDAO.findAll());
-        return "signin";
+
+    private boolean checkConnection(User user) {
+                    User UserConnected = UserDAO.findByMail(user.getMail());
+                    if(user.getMail() != null & user.getPassword() != null){
+                        if(UserConnected != null & UserConnected.getPassword().equals(user.getPassword())){
+                            return true;
+                        }
+                        else {
+                            return false;
+            }
+
+        }
+        else {
+            return false;
+        }
+    }
+
+    @PostMapping("/signin")
+    public RedirectView registration(@ModelAttribute User user, RedirectAttributes attrs) {
+        User newUser = new User();
+        newUser.setPseudo(user.getPseudo());
+        newUser.setMail(user.getMail());
+        newUser.setPassword(user.getPassword());
+        UserDAO.save(newUser);
+        attrs.addFlashAttribute("message", "New User added");
+        return new RedirectView("/login");
     }
 }
--- src/main/java/fr/takima/demo/repositories/UserDAO.java
+++ src/main/java/fr/takima/demo/repositories/UserDAO.java
@@ -10,4 +10,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface UserDAO extends CrudRepository<User, Long> {
+    User findByMail(String mail);
 }
