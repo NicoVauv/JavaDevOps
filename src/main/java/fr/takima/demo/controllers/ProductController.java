e8fe0e345ed09ccbffed6d40c268833cdf902000
Nicolas Vauvillier <nicolas.vauvillier@epfedu.fr>
2019-09-17 11:38:52 +0200

    Modele de données done

--- src/main/java/fr/takima/demo/controllers/ProductController.java
+++ src/main/java/fr/takima/demo/controllers/ProductController.java
@@ -1,11 +1,13 @@
 package fr.takima.demo.controllers;
 
+import fr.takima.demo.models.MyList;
 import fr.takima.demo.repositories.FridgeDAO;
 import fr.takima.demo.repositories.ListDAO;
 import fr.takima.demo.repositories.ProductDAO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  *
@@ -31,6 +33,28 @@ public class ProductController {
     m.addAttribute("mylist", ListDAO.findAll());
     m.addAttribute("myfridge", fridgeDAO.findAll());
       return "index";
+  }
+
+  // Release a product in my list
+  @PostMapping("/releaseProduct")
+  public String releaseProduct(@ModelAttribute MyList mylist, RedirectAttributes attrs) {
+    attrs.addFlashAttribute("message", "Your product has been released");
+   /* if(ListDAO.findById(mylist.getId()).get().getOnlist() != 0){
+      ListDAO.findById(mylist.getId()).get().setOnlist(mylist.getOnlist() - 1);
+    }
+    else {
+      ListDAO.delete(mylist);
+    }*/
+    return "index";
+  }
+
+  // Add a product in my list
+  @PostMapping("/addProduct")
+  public String addNewProduct(@ModelAttribute MyList myList, RedirectAttributes attrs) {
+    attrs.addFlashAttribute("message", "Your product has been added");
+    ListDAO.save(myList);
+    // ListDAO.findById(myList.getId()).get().setOnlist(myList.getOnlist() + 1);
+    return "index";
   }
 
   /*@PostMapping("/new")
